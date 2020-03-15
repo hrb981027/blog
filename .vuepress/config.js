3da044104fda04853b8a69d1b5c205853b05450b
hrb981027 <981027027@qq.com>
2020-03-16 01:16:52 +0800

    add 2020程序员找工作指南.md

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -11,6 +11,12 @@ module.exports = {
           ['/Linux/Linux设置虚拟内存', 'Linux设置虚拟内存'],
           ['/Linux/解决MariaDB无密码就可以登录的问题', '解决MariaDB无密码就可以登录的问题'],
         ]
+      },
+      {
+        title: '生活',
+        children: [
+          ['/Life/2020程序员找工作指南', '2020程序员找工作指南']
+        ]
       }
     ]
   }
